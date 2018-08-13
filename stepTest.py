da374727a53cc656d4fbf6317b26438495121bed
ashkan anousheh <ashkan.anousheh@gmail.com>
2018-08-13 12:00:32 +0100

    rotate and take photo

--- stepTest.py
+++ stepTest.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GP
 from time import sleep
-#from picamera import PiCamera
+from picamera import PiCamera
 
-
+Camera = PiCamera()
 GP.setmode(GP.BOARD)
 
 pins = [37,35,33,31]
@@ -22,24 +22,18 @@ halfStepSequence = [
   [1,0,0,1]
 ]
 
+j = 0
+
 for i in range(512):
 	for halfStep in range(8):
 		for pin in range(4):
 			GP.output(pins[pin],halfStepSequence[halfStep][pin])
 			sleep(0.001)
 
-	if i == 127 :
-		print('take picture1')
+	if (i% 31 == 0) :
 		sleep(1)
-	if i == 255 :
-		print('take picture2')
-		sleep(1)
-	if i == 383 :
-		print('take picture3')
-		sleep(1)
-	if i == 511 :
-		print('take picture4')
-		sleep(1)
-
+		Camera.capture('/home/pi/projects/stepperMotor/img/img%s.jpg'%j)
+		print('take picture %d' %(j))
+		j=j+1
 
 GP.cleanup()
